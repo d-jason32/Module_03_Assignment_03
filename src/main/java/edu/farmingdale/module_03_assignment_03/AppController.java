468f02a0745235e0d3c2f5c135fb5eb3497b53a7
Jason Devaraj <jdevaraj01@gmail.com>
2025-02-20 19:14:19 -0500

    Added code by Obye Shaji.

--- src/main/java/edu/farmingdale/module_03_assignment_03/AppController.java
+++ src/main/java/edu/farmingdale/module_03_assignment_03/AppController.java
@@ -1,33 +1,32 @@
 package edu.farmingdale.module_03_assignment_03;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
 
 public class AppController {
 
     public final static int JUMP = 20;
     @FXML
-    private Button autoCar1;
-
-    @FXML
-    private Button autoCar2;
-
-    @FXML
-    private Button autoRobot1;
-
-    @FXML
     private Button autoRobot2;
 
     @FXML
-    public ImageView movingImage;
+    private Button autoRobotButton;
+
+    @FXML
+    private ImageView movingImage;
 
     @FXML
     private ImageView movingImage1;
 
     @FXML
-    public Button restartButton1;
+    private Button restartButton1;
 
     @FXML
     private Button restartButton2;
@@ -44,7 +43,20 @@ public class AppController {
     @FXML
     private Button switchRobot2;
 
+    @FXML
+    void restart1(ActionEvent event) {
+        movingImage.setY(0);
+        movingImage.setX(0);
+    }
+    @FXML
+    void restart2(ActionEvent event) {
+        movingImage1.setY(0);
+        movingImage1.setX(0);
+    }
 
+    /**
+     * @author Jason Devaraj
+     */
     public void processKeyPress(KeyEvent event)
     {
         System.out.println("Key pressed: " + event.getCode());
@@ -75,5 +87,96 @@ public class AppController {
         }
         event.consume();
 
+    }
+
+    /**
+     * @author Obye Shaji
+     */
+    @FXML
+    public void autoRobot1() {
+        movingImage.setY(0);
+        movingImage.setX(0);
+
+        System.out.println("AutoSolve button pressed");
+        final String path = "RRUUUUUUUURRRRRRRRRRRRRRRRUUUURRRRDDDDDDDDDDDDDDDDRRRRUU" +
+                "UUUUUURRRRRRRRUUUUUUUURRRRRDDDDDDDDDDRRR";
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        for (int i = 0; i < path.length(); i++) {
+            final int index = i;
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(100 * (i + 1)),
+                    new EventHandler<ActionEvent>() {
+                        public void handle(ActionEvent e) {
+                            char move = path.charAt(index);
+                            System.out.println("Step " + (index + 1) + ": " + move);
+                            switch (move) {
+                                case 'R':
+                                    movingImage.setX(movingImage.getX() + JUMP);
+                                    break;
+                                case 'L':
+                                    movingImage.setX(movingImage.getX() - JUMP);
+                                    break;
+                                case 'U':
+                                    movingImage.setY(movingImage.getY() - JUMP);
+                                    break;
+                                case 'D':
+                                    movingImage.setY(movingImage.getY() + JUMP);
+                                    break;
+                                default:
+                                    System.out.println("Unknown move: " + move);
+                                    break;
+                            }
+                        }
+                    }
+            );
+            timeline.getKeyFrames().add(keyFrame);
+        }
+        timeline.play();
+
+    }
+    /**
+     * @author Obye Shaji
+     */
+    @FXML
+    public void autoRobot2() {
+        movingImage1.setY(0);
+        movingImage1.setX(0);
+        System.out.println("AutoSolve Maze 2 button pressed");
+        final String path = "DDDDDDDDDDDDDDDDDDDDDRRRRRRRRRRRRRUUUUUUUUUUUUURRRRRRRR" +
+                "RRRRUUUUUUUUUUURRRRRRRRRRDDDDDDDDDDDDDDDDDDDDDDDDD";
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(1);
+        for (int i = 0; i < path.length(); i++) {
+            final int index = i;
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(100 * (i + 1)),
+                    new EventHandler<ActionEvent>() {
+                        public void handle(ActionEvent e) {
+                            char move = path.charAt(index);
+                            System.out.println("Maze 2 Step " + (index + 1) + ": " + move);
+                            switch (move) {
+                                case 'R':
+                                    movingImage1.setX(movingImage1.getX() + JUMP);
+                                    break;
+                                case 'L':
+                                    movingImage1.setX(movingImage1.getX() - JUMP);
+                                    break;
+                                case 'U':
+                                    movingImage1.setY(movingImage1.getY() - JUMP);
+                                    break;
+                                case 'D':
+                                    movingImage1.setY(movingImage1.getY() + JUMP);
+                                    break;
+                                default:
+                                    System.out.println("Unknown move: " + move);
+                                    break;
+                            }
+                        }
+                    }
+            );
+            timeline.getKeyFrames().add(keyFrame);
+        }
+        timeline.play();
     }
 }
